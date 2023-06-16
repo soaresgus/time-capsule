e4c97e685f95ab51d80f3d54eba339de57d3ba73
soaresgus <gustavosoaresbt@hotmail.com>
2023-06-16 11:30:12 -0300

    feat: mobile: configure eslint and prettier

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
@@ -7,5 +7,5 @@ export default function App() {
       <Text>Hello World</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
