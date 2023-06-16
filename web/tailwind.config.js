24366e78c85f60e514f6cbd2405fa4673db207db
soaresgus <gustavosoaresbt@hotmail.com>
2023-06-16 11:42:37 -0300

    feat: web: configure fonts

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'var(--font-roboto)',
+        alt: 'var(--font-bai-jamjuree)',
+      },
+    },
   },
   plugins: [],
 }
