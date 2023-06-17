e873e5c0b794e21a41fc7e61172ccadb09bc0612
soaresgus <gustavosoaresbt@hotmail.com>
2023-06-17 11:19:29 -0300

    feat: mobile: configure fonts and splash screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,46 @@
+import { useCallback } from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground } from 'react-native'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import * as SplashScreen from 'expo-splash-screen'
+
+import blurBg from './src/assets/bg-blur.png'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (hasLoadedFonts) {
+      await SplashScreen.hideAsync()
+    }
+  }, [hasLoadedFonts])
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={blurBg}
+      onLayout={onLayoutRootView}
+      className="relative flex-1 items-center bg-gray-900"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StatusBar style="light" translucent />
+    </ImageBackground>
   )
 }
