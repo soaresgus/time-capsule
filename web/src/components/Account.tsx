604a4a685e8651da5da5b91be4b5d0e096ed9dd9
soaresgus <gustavosoaresbt@hotmail.com>
2023-06-19 16:15:16 -0300

    feat: web: implemented authentication

--- web/src/components/Account.tsx
+++ web/src/components/Account.tsx
@@ -1,19 +1,13 @@
-import { User } from 'lucide-react'
-import Link from 'next/link'
+import { cookies } from 'next/headers'
+import { SignIn } from './SignIn'
+import { Profile } from './Profile'
 
 export function Account() {
-  return (
-    <Link
-      href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
-      className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
-    >
-      <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-        <User className="h-5 w-5 text-gray-500" />
-      </div>
+  const isAuthenticated = cookies().has('token')
 
-      <p className="max-w-[140px] text-sm leading-snug">
-        <span className="underline">Crie sua conta</span> e salve suas memórias!
-      </p>
-    </Link>
-  )
+  if (!isAuthenticated) {
+    return <SignIn />
+  }
+
+  return <Profile />
 }
